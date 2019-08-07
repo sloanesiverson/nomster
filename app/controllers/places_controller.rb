0719b8abf1ece6148dd7b9d9723a4acc4388d1f9
Sloane Siverson <sloanems@aol.com>
2019-08-07 02:30:51 +0000

    updating the place data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,6 +23,12 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+
+    @place = Place.find(params[:id])
+
+  end
+
   private
 
   def place_params
