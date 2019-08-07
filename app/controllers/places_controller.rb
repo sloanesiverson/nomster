16019a63bcdce6bee7cb152e5253b94b90da0e61
Sloane Siverson <sloanems@aol.com>
2019-08-07 03:04:39 +0000

    added a delete button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -29,6 +29,18 @@ class PlacesController < ApplicationController
 
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+     redirect_to root_path
+  end
+
   private
 
   def place_params
