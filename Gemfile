abf441c8b144a6132bb27c03a4b6826fd4d309d9
Sloane <sloanems@aol.com>
2019-08-23 03:57:32 +0000

    email

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ gem "font-awesome-rails"
 gem 'devise'
 gem "geocoder"
 gem "figaro"
+gem 'carrierwave', '~> 1.0'
+gem 'fog-aws'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_224822) do
+ActiveRecord::Schema.define(version: 2019_08_21_210332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 2019_08_17_224822) do
   create_table "photos", force: :cascade do |t|
     t.text "caption"
     t.integer "place_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
   create_table "places", force: :cascade do |t|
